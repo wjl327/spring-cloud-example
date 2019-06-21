c04e719b02267946603a95c04a4d8b1f53b4708e
Jarvis.Wu <jarvis320700@gmail.com>
2019-06-21 15:07:01 +0800

    update config client

--- spring-cloud-config/spring-cloud-config-client/src/main/java/com/wjl327/config/client/controller/ConfigController.java
+++ spring-cloud-config/spring-cloud-config-client/src/main/java/com/wjl327/config/client/controller/ConfigController.java
@@ -2,19 +2,17 @@ package com.wjl327.config.client.controller;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/config")
 public class ConfigController {
 
-    @Value("${serverName}")
-    private String serverName;
+	@Value("${serverName}")
+	String serverName;
 
-    @RequestMapping(value = "/hello", method = RequestMethod.GET)
-    public String hello() {
-        return "Config value: " + serverName;
-    }
-
+	@RequestMapping(value = "/hello")
+	public String hello(){
+		return serverName;
+	}
 }
