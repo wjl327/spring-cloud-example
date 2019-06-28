971b8b2070c09cf02df8aef1e2fd932755f41c58
Jarvis.Wu <jarvis320700@gmail.com>
2019-06-28 23:50:20 +0800

    update sleuth zipkin example

--- spring-cloud-sleuth/spring-cloud-zipkin-zdb/src/main/java/com/wjl327/zipkin/zdb/SpringCloudZipkinZdbApplication.java
+++ spring-cloud-sleuth/spring-cloud-zipkin-zdb/src/main/java/com/wjl327/zipkin/zdb/SpringCloudZipkinZdbApplication.java
@@ -2,8 +2,10 @@ package com.wjl327.zipkin.zdb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class SpringCloudZipkinZdbApplication {
 
     public static void main(String[] args) {
---
+++ spring-cloud-sleuth/spring-cloud-zipkin-zdb/src/main/java/com/wjl327/zipkin/zdb/controller/ZdbController.java
@@ -0,0 +1,37 @@
+package com.wjl327.zipkin.zdb.controller;
+
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
+@RestController
+@RequestMapping("/zdb")
+public class ZdbController {
+
+    private Map<String, String> users;
+
+    public ZdbController() {
+        users = new HashMap<>();
+        users.put("xiaoMi", "XiaoMi/21/S/160/student");
+        users.put("xiaoHei", "XiaoHei/22/M/175/student");
+        users.put("dowson", "Dowson/40/M/178/teacher");
+    }
+
+    @RequestMapping(value = "/getUserData", method = RequestMethod.GET)
+    public String GetUserData(String name) {
+        int cost = new Random().nextInt(1000);
+        try {
+            Thread.sleep(cost);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return users.get(name);
+    }
+
+}
