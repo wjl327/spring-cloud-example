0bd01009e15eeb3abb9befa0319047b34cf829ea
Jarvis.Wu <jarvis320700@gmail.com>
2019-06-30 09:02:37 +0800

    update hystrix dashboard example

--- spring-cloud-netflix-hystrix/spring-cloud-feign-hystrix-consumer/src/main/java/com/wjl/spring/hystrix/consumer/SpringCloudEurekaFeignConsumerApplication.java
+++ spring-cloud-netflix-hystrix/spring-cloud-feign-hystrix-consumer/src/main/java/com/wjl/spring/hystrix/consumer/SpringCloudEurekaFeignConsumerApplication.java
@@ -2,9 +2,17 @@ package com.wjl.spring.hystrix.consumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableHystrix
+@EnableHystrixDashboard
+@EnableCircuitBreaker
 @EnableFeignClients
 public class SpringCloudEurekaFeignConsumerApplication {
 
