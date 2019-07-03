8bb6704d4531ef1dc1936984679c4edca5b9ca9f
Jarvis.Wu <jarvis320700@gmail.com>
2019-07-03 23:46:06 +0800

    update spring cloud gateway example

--- spring-cloud-gateway/spring-cloud-gateway-example/src/main/java/com/wjl327/gateway/SpringCloudGatewayExampleApplication.java
+++ spring-cloud-gateway/spring-cloud-gateway-example/src/main/java/com/wjl327/gateway/SpringCloudGatewayExampleApplication.java
@@ -2,7 +2,10 @@ package com.wjl327.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -10,6 +13,14 @@ public class SpringCloudGatewayExampleApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringCloudGatewayExampleApplication.class, args);
+    }
+
+    @Bean
+    public RouteLocator myRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(r -> r.path("/feign/**").filters(f->f.stripPrefix(1)).uri("lb://spring-cloud-eureka-feign-consumer"))
+                .route(r -> r.path("/ribbon/**").filters(f->f.stripPrefix(1)).uri("lb://spring-cloud-eureka-ribbon-consumer"))
+                .build();
     }
 
 }
